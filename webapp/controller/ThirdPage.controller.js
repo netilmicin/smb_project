f9df3c106320731010557f8d2587b7e633c84397
Yannik Tobler <yannik.tobler@agilita.ch>
2022-09-19 16:07:16 +0200

    controller and view commits

--- webapp/controller/ThirdPage.controller.js
+++ webapp/controller/ThirdPage.controller.js
@@ -1,10 +1,26 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+	"sap/f/library",
+	"sap/ui/core/Fragment"
 ], function(
 	Controller
 ) {
 	"use strict";
 
 	return Controller.extend("smbproject1.0.controller.ThirdPage", {
+		getPage : function() {
+			return this.byId("dynamicPageId");
+		},
+		toggleAreaPriority : function () {
+			var oTitle = this.getPage().getTitle(),
+				sNewPrimaryArea = oTitle.getPrimaryArea() === DynamicPageTitleArea.Begin ? DynamicPageTitleArea.Middle : DynamicPageTitleArea.Begin;
+			oTitle.setPrimaryArea(sNewPrimaryArea);
+		},
+		/* deleteArea : function () {
+			for (let i = 0; i < $orders.length; i++){
+				delete $orders[i].position;
+			}
+		} */
 	});
 });
