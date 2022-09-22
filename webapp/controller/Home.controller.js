9e7247c61abf33a3ec87a9ba28176ac5efd973fc
Andrej Klimov <andrej.klimov@agilita.ch>
2022-09-22 16:23:55 +0200

    logic for first page

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,22 +1,34 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/base/strings/formatMessage"],
-  function (Controller) {
-    "use strict";
-    return Controller.extend("smbproject1.0.controller.Home", {
-      /*   onNavToConfiguration: function() {
-            this.getRouter().navTo("configuration");
-        }, */
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/base/strings/formatMessage",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/FilterOperator",
+  ],
 
-        /**
-         * @override
-         */
-         /* onInit: function() {
-          Controller.prototype.onInit.apply(this, arguments);
-          const obj = JSON.parse(orders);
-          console.log(obj.OverallDeliveryStatus);
-        },  */
+  function (Controller, formatMessage, JSONModel, FilterOperator) {
+    "use strict";
+
+    return Controller.extend("smbproject1.0.controller.Home", {
+      /**
+       * @override
+       */
+      onInit: function () {
+        Controller.prototype.onInit.apply(this, arguments);
+        let oSalesOfficeModel = new JSONModel("model/mockdata.json");
+        let setOfSalesOffices = new Set();
+        oSalesOfficeModel.forEach((element) => {
+          setOfSalesOffices.add(element.Orders>SalesOffice);
+        });
+        setOfSalesOffices.forEach((element) => {
+          console.log(element);
+        });
+        
+      },
+
       onChartPressed: function (oEvent) {
         let oRouter = this.getOwnerComponent().getRouter();
+
         oRouter.navTo("secondPage", {
           location: oEvent.getSource().getTitle(),
         });
