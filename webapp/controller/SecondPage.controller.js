b3fefa32ef8b1d6c02a8e3f172f1fd69dd85f91b
Yannik Tobler <yannik.tobler@agilita.ch>
2022-09-22 14:53:29 +0200

    changes to filters

--- webapp/controller/SecondPage.controller.js
+++ webapp/controller/SecondPage.controller.js
@@ -9,31 +9,35 @@ sap.ui.define(
     "sap/base/util/UriParameters",
     "sap/ui/model/FilterOperator",
     "sap/ui/model/FilterType",
+	"sap/ui/core/format/NumberFormat",
   ],
   function (
     Log,
-    Controller,
-    JSONModel,
-    MessageToast,
-    DateFormat,
-    Filter,
-    UriParameters,
-    FilterOperator,
-    FilterType
+	Controller,
+	JSONModel,
+	MessageToast,
+	DateFormat,
+	Filter,
+	UriParameters,
+	FilterOperator,
+	FilterType,
+	NumberFormat
   ) {
     "use strict";
-
     return Controller.extend("smbproject1.0.controller.SecondPage", {
+      _filters: [],
+
+      _sLocation: "",
+      _sStatus: "",
+
       _onObjectMatched: function (oEvent) {
         let location = oEvent.getParameter("arguments").location;
         this.getView().byId("secondPageTitle").setText(location);
         let oView = this.getView();
-        let sValue = oEvent.getParameter("arguments").location;
-        let oFilter = new Filter("SalesOffice", FilterOperator.EQ, sValue);
-        oView
-          .byId("orderTable")
-          .getBinding("items")
-          .filter(oFilter, FilterType.Application);
+
+        this._sLocation = location;
+
+        this._applyFilters();
       },
       onInit: function () {
         // set explored app's demo model on this sample
@@ -44,8 +48,6 @@ sap.ui.define(
         oRouter
           .getRoute("secondPage")
           .attachMatched(this._onObjectMatched, this);
-
-        
       },
 
       onPaste: function (oEvent) {
@@ -57,24 +59,44 @@ sap.ui.define(
         let oTable = this.byId("orderTable");
         let oComboBox = this.byId("idSelectStatus");
         let chosenKey = oComboBox.getSelectedKey();
-        switch (chosenKey) {
-          case "Ausgeführt":
-            break;
-        }
-      },
+        console.log(chosenKey);
+        let oView = this.getView();
 
-      onPressBackButton: function (oEvent) {
-        let oRouter = this.getOwnerComponent().getRouter();
-        oRouter.navTo("home", {});
+        this._sStatus = chosenKey;
+
+        this._applyFilters();
       },
 
       onRowPressed: function (oEvent) {
         let oItem = oEvent.getSource().getBindingContext("orders").getPath();
-
         let oRouter = this.getOwnerComponent().getRouter();
         oRouter.navTo("thirdPage", {
           orders: window.encodeURIComponent(oItem),
         });
+      },
+
+      _applyFilters() {
+        const aFilters = [];
+
+        if (this._sLocation) {
+          aFilters.push(
+            new Filter("SalesOffice", FilterOperator.EQ, this._sLocation)
+          );
+        }
+
+        if (this._sStatus) {
+          aFilters.push(
+            new Filter(
+              "OverallDeliveryStatus",
+              FilterOperator.EQ,
+              this._sStatus
+            )
+          );
+        }
+        this.getView()
+          .byId("orderTable")
+          .getBinding("items")
+          .filter(aFilters, FilterType.Application);
       },
     });
   }
